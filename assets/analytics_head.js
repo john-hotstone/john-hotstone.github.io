051d53df65ff27fccaca0a9018da7eedddbdf7ee
Seraph649 <105313614+Seraph649@users.noreply.github.com>
2025-03-19 11:39:26 +0100

    Change cookie banner and google analytics

---
+++ assets/analytics_head.js
@@ -0,0 +1,6 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ site.google_analytics_ga4 }}"></script>
+<script>
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', '{{ site.google_analytics_ga4 }}');
+</script>
